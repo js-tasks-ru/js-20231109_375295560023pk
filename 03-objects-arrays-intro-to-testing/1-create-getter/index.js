b8b749fe30a49dad96b7ba2fedf0a7704879b54d
Pavel Khmara <375295560023pk@gmail.com>
2023-11-16 21:17:31 +0100

    03-01 createGetter function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,11 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    return function(obj) {
+        for (let key of path.split('.')) {
+            obj = obj?.[key]
+        }
+        
+        return obj 
+    }
 }
